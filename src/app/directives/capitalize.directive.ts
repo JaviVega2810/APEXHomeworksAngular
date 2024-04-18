542f1360e6ea5e0d3c45e2579c95e9f84c1d952f
JaviVega2810 <javiervega2810@gmail.com>
2024-04-18 15:40:55 -0600

    I've finished homework number 3

---
+++ src/app/directives/capitalize.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[capitalize]'
+})
+export class CapitalizeDirective {
+
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.style.textTransform = "capitalize";
+  }
+
+}
