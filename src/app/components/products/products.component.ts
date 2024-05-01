ef9de2f9aac4edc7987952438d7664473da7d1d5
JaviVega2810 <javiervega2810@gmail.com>
2024-05-01 13:26:25 -0600

    changes to set grid list for products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HardCodeProductsService } from '../../services/hard-code-products.service';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+
+  public constructor(private readonly products: HardCodeProductsService){}
+
+  public items: Product[] = this.products.getItems();
+
+
 
 }
