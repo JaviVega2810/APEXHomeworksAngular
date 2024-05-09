b3cfe27e913b49d8de9f36b8a8f7a0fa7a61b742
JaviVega2810 <jgv283@gmail.com>
2024-05-09 17:57:37 -0600

    added function for component individual for product

--- src/app/components/products/single/single.component.ts
+++ src/app/components/products/single/single.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HardCodeProductsService } from '../../../services/hard-code-products.service';
+import { Product } from '../../../interfaces/product';
 
 @Component({
   selector: 'app-single',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './single.component.scss'
 })
 export class SingleComponent {
+
+  public param: string = ''
+  public item!: Product;
+
+  public constructor(
+                     private route: ActivatedRoute,
+                     private readonly productsService: HardCodeProductsService
+                    ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.param = params['id'];
+    });
+    this. item = this.productsService.getItemById(this.param);
+  }
 
 }
