4633b0a27e881010a91a40c9c311b96f685790aa
JaviVega2810 <jgv283@gmail.com>
2024-05-17 12:22:42 -0600

    clean code and remove empty constructors

--- src/app/services/random.service.ts
+++ src/app/services/random.service.ts
@@ -2,14 +2,9 @@ import { Injectable } from '@angular/core';
 import { ProviderInjectableModule } from '../provider-injectable-module/provider-injectable.module';
 
 @Injectable({
-
   providedIn: ProviderInjectableModule
-
 })
 
 export class RandomService {
-
   public data: string[] = [ 'one', 'two' ];
-
-  constructor() { }
 }
